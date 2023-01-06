79673ac2e61c001d7621b43ddfe2197e717638db
syunaev <safiya.yunaeva@gmail.com>
2023-01-05 23:04:51 -0800

    Initial commit: Leetcode blind 75

--- leetcode75easy.py
+++ leetcode75easy.py
@@ -3,9 +3,9 @@ def two_sum(nums, target):
         Given an array of integers nums and an integer target,
         return indices of the two numbers such that they add up to target.
     """
-    values = {} #value : index
+    values = {}  # value : index
     for i in range(len(nums)):
         second_value = target - nums[i]
         if second_value in values:
-            return[values.get(second_value), i]
+            return [values.get(second_value), i]
         values[nums[i]] = i
