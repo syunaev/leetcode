a36f70d45898f25851054fc0df322f9f672badaf
syunaev <safiya.yunaeva@gmail.com>
2023-01-15 06:34:09 +0100

    Leetcode blind 75: Len of longest substring

--- leetcode75easy.py
+++ leetcode75easy.py
@@ -23,3 +23,29 @@ def runningSum(nums: List[int]) -> List[int]:
 
     return result
 
+def len_of_longest_substring(s):
+    """ Given a string s, find the length of the longest
+        substring without repeating characters."""
+    # https://www.youtube.com/watch?v=qqXOZD4zKEg
+    """
+    1. start for loop from first char
+    2. init empty set
+    3. while not exist - increment count
+    4. return
+    O(N^2)
+    """
+    result = 0
+    for i in range(len(s)):
+        seen = set()
+        char = i
+        counter = 0
+        while char < len(s):
+            if s[char] not in seen:
+                seen.add(s[char])
+                counter = counter + 1
+                char = char + 1
+            else:
+                break
+        if counter > result:
+            result = counter
+    return result
